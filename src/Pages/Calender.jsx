1e3c92743c1652fc72521d133577ae28259e6f89
Sanghamitra Satpathy <sanghamitrasatpathy@Sanghamitras-MacBook-Pro.local>
2024-06-01 19:50:52 +0530

    updated github calendar

--- src/Pages/Calender.jsx
+++ src/Pages/Calender.jsx
@@ -8,7 +8,7 @@ function Calender() {
   const { colorMode } = useColorMode();
   return (
     <Box className="react-activity-calendar" bg={colorMode==="light"? "linear-gradient(180deg, rgba(44,23,75,1) 45%, rgba(28,14,50,1) 99%)" : "#bdb6c9"} color={colorMode==="light"? "white" : "#13022C"} pb="60px">
-        <Box w="80%" transform="scale(1.05)" m="auto" textAlign="center" p="60px auto">
+        <Box w="80%" transform="scale(1.05)" m="auto" display="flex" justifyContent="center" p="60px auto">
             <Row style={{ justifyContent: "center", padding: "20px auto", paddingTop: "60px"}}>
             <Text mb="25px" textAlign="center"  fontWeight="400" fontSize={["2xl", "4xl", "5xl"]}>My Github Calender</Text>
             <GitHubCalendar
